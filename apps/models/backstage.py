17f86fe4288a4dca4dd405da2b30313553fa3a27
panla <pankla@163.com>
2021-02-07 13:40:51 +0800

    add and update apps/models/backstage.py 补充 property

--- apps/models/backstage.py
+++ apps/models/backstage.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import validates
 from sqlalchemy import text
+from flask_sqlalchemy import BaseQuery
 
 from apps.db import db
 from apps.mixin.model import BaseModel, ModelMixin
@@ -19,6 +20,8 @@ class BackstagePermission(db.Model, ModelMixin):
 
     @validates('name')
     def validate_name(self, key, value):
+        """校验权限名称"""
+
         if BackstagePermission.query.filter(
                 BackstagePermission.id != self.id, BackstagePermission.name == value).first():
             raise Exception('该权限名称已存在')
@@ -36,8 +39,23 @@ class BackstageRole(db.Model, ModelMixin):
 
     @validates('name')
     def validate_name(self, key, value):
+        """校验角色名称"""
+
         if BackstageRole.query.filter(BackstageRole.id != self.id, BackstageRole.name == value).first():
             raise Exception('该角色名称已存在')
+
+    @property
+    def permission_ids(self) -> list:
+        """所拥有的权限id"""
+        return self.permissions_set.split(',')
+
+    @property
+    def permissions(self) -> BaseQuery:
+        """所拥有的权限"""
+
+        _permission_ids = self.permissions_set.split(',')
+        return BackstagePermission.query.filter(
+            BackstagePermission.id.in_(_permission_ids), BackstagePermission.is_delete.is_(False))
 
 
 class BackstageAccount(db.Model, ModelMixin):
@@ -49,3 +67,16 @@ class BackstageAccount(db.Model, ModelMixin):
     account_id = db.Column(UNSIGNED_BIGINTEGER, unique=True, nullable=False, comment='统一账户')
     role_id = db.Column(db.Integer, nullable=False, comment='角色id')
     is_staff = db.Column(db.Boolean, nullable=False, server_default=text('0'), comment='是否是平台方账号')
+
+    @property
+    def role(self) -> BaseQuery:
+        """所属角色"""
+
+        return BackstageRole.get(id=self.role_id, is_delete=False)
+
+    @property
+    def account(self) -> BaseQuery:
+        """统一账户"""
+
+        from apps.models import Account
+        return Account.get(id=self.account_id, is_delete=False)
