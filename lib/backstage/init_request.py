6316e8c05ee357052beab6ef4c07e74ed370823a
panla <pankla@163.com>
2021-02-08 15:07:37 +0800

    update and delete 请求前的权限验证

--- lib/backstage/init_request.py
+++ lib/backstage/init_request.py
@@ -16,6 +16,9 @@ def identify():
     header_token = request.headers.get('X-TOKEN')
     if header_token:
         payload = decode_auth_token(header_token)
+        if not g.b_account.role.permissions.filter_by(
+                endpoint=request.endpoint.split('.')[-1], method=request.method).first():
+            return responses(**ErrorCode.no_access)
         if isinstance(payload, dict) and payload.get('data') and payload.get('data').get('id'):
             return None
         return responses(**ErrorCode.token_decode_error)
