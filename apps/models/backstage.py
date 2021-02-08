736bece826439c52b497c037e9240c44eaeead4a
panla <pankla@163.com>
2021-02-08 14:21:56 +0800

    add Model ApiPermission

--- apps/models/backstage.py
+++ apps/models/backstage.py
@@ -28,6 +28,29 @@ class BackstagePermission(db.Model, ModelMixin):
         return value
 
 
+class ApiPermission(db.Model, ModelMixin):
+
+    __tablename__ = 'api_permissions'
+    __table_args__ = ({'comment': '接口权限表'})
+
+    id = db.Column(UNSIGNED_SMALLINT, primary_key=True)
+    endpoint = db.Column(db.String(300), nullable=False, unique=True, comment='endpoint')
+    methods = db.Column(db.String(50), nullable=False, comment='请求方法,')
+    is_delete = db.Column(db.Boolean, nullable=False, server_default=text('0'), comment='删除标志')
+
+    @validates('endpoint')
+    def validate_endpoint(self, key, value):
+        """校验权限名称"""
+
+        if ApiPermission.query.filter(ApiPermission.id != self.id, ApiPermission.endpoint == value).first():
+            raise Exception('该endpoint已存在')
+        return value
+
+    @property
+    def methods_(self):
+        return self.methods.split(',')
+
+
 class BackstageRole(db.Model, ModelMixin):
 
     __tablename__ = 'backstage_roles'
