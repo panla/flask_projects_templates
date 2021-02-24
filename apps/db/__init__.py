01b5cbdb39b2da6c272db7c5fc581469e095be17
panla <pankla@163.com>
2021-02-24 09:12:32 +0800

    update 完善apps/db/__init__.py

--- apps/db/__init__.py
+++ apps/db/__init__.py
@@ -9,12 +9,14 @@ class RoutingSession(SignallingSession):
 
         if self._flushing:
             # 写操作 ,使用主数据库
+            # self.app.logger.info('write data into master database')
             return state.db.get_engine(self.app)
         else:
             # 读操作, 使用从数据库
-            # 目前还没有研究主从复制，就先不搞这个了
+            # 目前还没有研究mysql主从同步，就先不搞这个了
+            # self.app.logger.info('read data from slave database')
             # return state.db.get_engine(self.app, bind='slave')
-            return state.db.get_engine(self.app, bind='slave')
+            return state.db.get_engine(self.app)
 
 
 class RoutingSQLAlchemy(SQLAlchemy):
