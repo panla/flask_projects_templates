37acb90932141ef2e200f1d1edba14cd3c867b3c
panla <pankla@163.com>
2021-03-07 19:54:22 +0800

    style models/account.py backstage.py

--- apps/models/account.py
+++ apps/models/account.py
@@ -6,15 +6,15 @@ from apps.mixin.model import BaseModel, ModelMixin
 
 
 class Account(BaseModel, ModelMixin):
-    __tablename__ = 'accounts'
-    __table_args__ = ({'comment': '账户表'})
-
     cellphone = db.Column(db.String(12), unique=True, nullable=False, comment='手机号')
     name = db.Column(db.String(50), index=True, nullable=False, server_default='', comment='姓名')
     nickname = db.Column(db.String(50), nullable=False, server_default='', comment='昵称')
     password_hash = db.Column(db.String(300), nullable=False, comment='密码加密')
     login_at = db.Column(db.DateTime, comment='登录时间')
     login_expired_at = db.Column(db.DateTime, comment='登录过期时间')
+
+    __tablename__ = 'accounts'
+    __table_args__ = ({'comment': '账户表'})
 
     @validates('cellphone')
     def validate_cellphone(self, key, value):
--- apps/models/backstage.py
+++ apps/models/backstage.py
@@ -1,21 +1,21 @@
-from sqlalchemy.orm import validates
 from sqlalchemy import text
 from flask_sqlalchemy import BaseQuery
+from sqlalchemy.orm import validates
 
 from apps.db import db
-from apps.mixin.model import BaseModel, ModelMixin
-from apps.mixin.model import UNSIGNED_BIGINTEGER, UNSIGNED_INTEGER, UNSIGNED_SMALLINT
+from apps.mixin.model import ModelMixin
+from apps.mixin.model import UNSIGNED_BIGINTEGER, UNSIGNED_INTEGER
 
 
 class BackstageRole(db.Model, ModelMixin):
-    __tablename__ = 'backstage_roles'
-    __table_args__ = ({'comment': '后台管理系统角色表'})
-
     id = db.Column(UNSIGNED_INTEGER, autoincrement=True, primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=True, comment='角色名称')
     desc = db.Column(db.String(100), nullable=False, comment='角色描述')
     permissions_set = db.Column(db.String(500), nullable=False, comment='权限id,以,分割')
     is_delete = db.Column(db.Boolean, nullable=False, server_default=text('0'), comment='删除标志')
+
+    __tablename__ = 'backstage_roles'
+    __table_args__ = ({'comment': '后台管理系统角色表'})
 
     @validates('name')
     def validate_name(self, key, value):
@@ -44,13 +44,13 @@ class BackstageRole(db.Model, ModelMixin):
 
 
 class BackstageAccount(db.Model, ModelMixin):
-    __tablename__ = 'backstage_accounts'
-    __table_args__ = ({'comment': '后台管理系统账户表'})
-
     id = db.Column(UNSIGNED_BIGINTEGER, autoincrement=True, primary_key=True)
     account_id = db.Column(UNSIGNED_BIGINTEGER, unique=True, nullable=False, comment='统一账户')
     role_id = db.Column(UNSIGNED_INTEGER, index=True, nullable=False, comment='角色id')
     is_staff = db.Column(db.Boolean, nullable=False, server_default=text('0'), comment='是否是平台方账号')
+
+    __tablename__ = 'backstage_accounts'
+    __table_args__ = ({'comment': '后台管理系统账户表'})
 
     @property
     def role(self) -> BaseQuery:
